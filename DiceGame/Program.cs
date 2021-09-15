e6e4db77db93d6984594eee0fb59a1300fb556dd
Opilane TTHK <opilane@tthk.ee>
2021-09-15 09:32:52 +0300

    Random Number Generator

---
+++ DiceGame/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace DiceGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //mängivad 2 mängijat
+            //mõlemad mängijad viskavad täringut
+            //programm kontrollib, kes rohkem viskas
+            //programm kuulutab võitja
+
+            Random rnd = new Random();
+            int dice = rnd.Next(1, 7);
+            int dice2 = rnd.Next(1, 7);
+
+            Console.WriteLine("Vajuta [Enter], et täringut visata[Player1]");
+            Console.ReadLine();
+            Console.WriteLine($"Sa veeretasid {dice}");
+
+            Console.WriteLine("Vajuta [Enter], et täringut visata[Player2]");
+            Console.ReadLine();
+            Console.WriteLine($"Sa veeretasid {dice2}");
+
+            if (dice > dice2)
+            {
+                Console.WriteLine("Võitja on [Player1]");
+            }
+            else
+            {
+                Console.WriteLine("Võitja on [Player2]");
+            }
+            Console.ReadLine();
+
+
+        }
+    }
+}
---
+++ RandomNumberGenerators/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace RandomNumberGenerators
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            int rndnumber = rnd.Next(1, 11); //number 1 kuni 10
+
+            Console.WriteLine($"Sinu juhuslik number on {rndnumber}");
+
+            //programm kontrollib, kas genereeritud number on suurem kui 5
+            //kui genereeritud number > 5, siis programm kuvab "..., see on suurem, kui 5
+            //kui on väiksem, siis programm kuvab "..., see on väiksem,kui 5
+            //(< - väiksem kui) , (> - suurem kui) , (== - võrdne)
+
+            if (rndnumber > 5)
+            {
+                Console.WriteLine($"Sinu juhuslik number on {rndnumber}, see on suurem kui 5");
+            }
+            else if (rndnumber < 5)
+            {
+                Console.WriteLine($"Sinu juhuslik number on {rndnumber}, see on väiksem kui 5");
+            }
+            else
+            {
+                Console.WriteLine($"Sinu juhuslik number on {rndnumber}");
+            }
+            Console.ReadLine();
+        }
+    }
+}
